0e637ae61ccc71ead309fb08279601cfa685365b
Skyler Torian <skyler.torian@yahoo.com>
2020-03-18 10:47:02 -0400

    created state for sign up form

---
+++ src/containers/SignUp.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+class SignUp extends React.Component {
+
+    state={
+        name: name,
+        userName: userName,
+        bio: bio,
+        profilePic: profilePic
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit>
+                    <textarea></textarea>
+                    <textarea></textarea>
+                    <textarea></textarea>
+                </form>
+
+
+            </div>
+        )
+    }
+}
+
+export default SignUp 
