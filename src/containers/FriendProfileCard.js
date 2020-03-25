baccd2417a0823085ab1ce4cd483c7872e5f5bf4
Skyler Torian <skyler.torian@yahoo.com>
2020-03-25 15:46:56 -0400

    fixed gamecube not existing on back end

---
+++ src/containers/FriendProfileCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class FriendProfileCard extends React.Component {
+    render() {
+        return (
+            <div>It's me! Your friend!</div>
+        )
+    }
+}
+
+export default FriendProfileCard 
