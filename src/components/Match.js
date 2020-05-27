92326ff988b7a73b827e169b6d7acac912ecd161
Skyler Torian <skyler.torian@yahoo.com>
2020-05-27 15:36:17 -0400

    added styling for user match photo

--- src/components/Match.js
+++ src/components/Match.js
@@ -59,8 +59,9 @@ class Match extends React.Component {
                     <h3 className="friend-clicker">
                         Click Find a Friend to get started:
                     </h3>
-                    <div>
-                    <input type="button" value="Find Friend" onClick={this.findFriend}></input>
+                    <div className="find-friend">
+                    <input type="button" 
+                     value="Find Friend" onClick={this.findFriend}></input>
                     </div>
                     <div>
                         {this.state.renderedFriend?
